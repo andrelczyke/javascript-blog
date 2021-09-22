6b510e136a21a2edbff50ccaa5e0959e03a39cd0
Elwira Andrelczyk <andrelczyke@gmail.com>
2021-09-22 18:19:24 +0200

    5 module

--- js/script.js
+++ js/script.js
@@ -9,18 +9,21 @@ function titleClickHandler(event){
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-  /* add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
+  /* add class 'active' to the clicked link - Mamy już odniesienie do klikniętego linka, zapisane w stałej clickedElement. Twoim zadaniem jest dodanie mu klasy active.*/
     const clickedElement = this;
+    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts a.active');
+  /* remove class 'active' from all articles - Zmień selektor .titles a.active na odpowiedni dla artykułów znajdujących się w naszym blogu.*/
+    const activeArticles = document.querySelectorAll('article.post.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
   /* get 'href' attribute from the clicked link */
+    const hrefValue = clickedElement.getAttribute('href').replace('#','');
+    const articleToActivate = document.getElementById(hrefValue);
+    articleToActivate.classList.add('active');
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
